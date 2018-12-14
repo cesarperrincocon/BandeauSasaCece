2491391e3415068e1b82471188924914c9a895a2
cperrinc <cperrinc@cst-tp-b10112>
2018-12-14 08:43:40 +0100

    Exemple utilisation modif

--- src/bandeau/ExempleDUtilisation.java
+++ src/bandeau/ExempleDUtilisation.java
@@ -4,57 +4,68 @@ import java.awt.Color;
 import java.awt.Font;
 
 public class ExempleDUtilisation {
-    private final Bandeau monBandeau = new Bandeau();
-    
+
+    private static Bandeau monBandeau = new Bandeau();
+
     public static void main(String[] args) {
-	    new ExempleDUtilisation().exemple();
+        Scenario scenario = new Scenario(monBandeau);
+        Effet_couleur maCouleur = new Effet_couleur(monBandeau);
+        Effet_rotation maRotation = new Effet_rotation(monBandeau, "IL EST DE RETOUR...");        
+        Effet_zoom monZoom = new Effet_zoom(monBandeau, "EN TOURNEE DANS TOUTE LA FRANCE !");
+        Effet_zoom monZoom = new Effet_zoom(monBandeau, "JUL");
+        Effet_clignotement monClignotement = new Effet_clignotement(monBandeau, "BORDEAUX");
+        scenario.ajouterEffet(maCouleur);
+        scenario.ajouterEffet(maRotation);
+        scenario.ajouterEffet(monZoom);
+        scenario.ajouterEffet(monClignotement);
+        scenario.jouerScenario();
     }
-    
+
+
     public void exemple() {
         Font font = monBandeau.getFont();
         Color back = monBandeau.getBackground();
         Color fore = monBandeau.getForeground();
 
-                monBandeau.setMessage("Hello");
-		monBandeau.sleep(1000);
-		monBandeau.setMessage("On va changer de police");
-		monBandeau.sleep(1000);
-		monBandeau.setMessage("Monospaced 15 Bold");
-		monBandeau.setFont(new Font("Monospaced", Font.BOLD, 15));
-		monBandeau.sleep(1000);
-		monBandeau.setMessage("SansSerif 15");
-		monBandeau.setFont(new Font("SansSerif", Font.PLAIN, 15));
-		monBandeau.sleep(1000);
-		monBandeau.setMessage("On va zoomer");
-		monBandeau.sleep(1000);
-		monBandeau.setMessage("Zoom........");
-		for (int i = 5; i < 60 ; i+=5) {
-			monBandeau.setFont(new Font("Dialog", Font.BOLD, 5+i));
-			monBandeau.sleep(100);
-		}
-		monBandeau.sleep(1000);
+        monBandeau.setMessage("Hello");
+        monBandeau.sleep(1000);
+        monBandeau.setMessage("On va changer de police");
+        monBandeau.sleep(1000);
+        monBandeau.setMessage("Monospaced 15 Bold");
+        monBandeau.setFont(new Font("Monospaced", Font.BOLD, 15));
+        monBandeau.sleep(1000);
+        monBandeau.setMessage("SansSerif 15");
+        monBandeau.setFont(new Font("SansSerif", Font.PLAIN, 15));
+        monBandeau.sleep(1000);
+        monBandeau.setMessage("On va zoomer");
+        monBandeau.sleep(1000);
+        monBandeau.setMessage("Zoom........");
+        for (int i = 5; i < 60; i += 5) {
+            monBandeau.setFont(new Font("Dialog", Font.BOLD, 5 + i));
+            monBandeau.sleep(100);
+        }
+        monBandeau.sleep(1000);
 
-		monBandeau.setFont(new Font("Courier new", Font.PLAIN, 15));
-		monBandeau.setMessage("On va tourner");
-		monBandeau.sleep(1000);
-		monBandeau.setMessage("On tourne...");
-		for (int i = 0; i <= 100; i++) {
-			monBandeau.setRotation(2*Math.PI*i / 100);
-			monBandeau.sleep(100);
-		}
+        monBandeau.setFont(new Font("Courier new", Font.PLAIN, 15));
+        monBandeau.setMessage("On va tourner");
+        monBandeau.sleep(1000);
+        monBandeau.setMessage("On tourne...");
+        for (int i = 0; i <= 100; i++) {
+            monBandeau.setRotation(2 * Math.PI * i / 100);
+            monBandeau.sleep(100);
+        }
 
-		monBandeau.setMessage("On va changer de couleur de fond");
-		monBandeau.sleep(1000);
-		monBandeau.setBackground(Color.DARK_GRAY);
-		monBandeau.setMessage("On va changer de couleur");
-		monBandeau.sleep(1000);
-		monBandeau.setForeground(Color.YELLOW);
-		monBandeau.sleep(1000);
-		monBandeau.setFont(font);
-		monBandeau.setForeground(fore);
-		monBandeau.setBackground(back);
-		monBandeau.setMessage("Termine");
-		//System.exit(0);
-   }
-} 
-
+        monBandeau.setMessage("On va changer de couleur de fond");
+        monBandeau.sleep(1000);
+        monBandeau.setBackground(Color.DARK_GRAY);
+        monBandeau.setMessage("On va changer de couleur");
+        monBandeau.sleep(1000);
+        monBandeau.setForeground(Color.YELLOW);
+        monBandeau.sleep(1000);
+        monBandeau.setFont(font);
+        monBandeau.setForeground(fore);
+        monBandeau.setBackground(back);
+        monBandeau.setMessage("Termine");
+        //System.exit(0);
+    }
+}
